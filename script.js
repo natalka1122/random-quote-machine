c178d73447e8061c58f16f4df87bc8698f5b7c7c
unknown <user@kalashnikova.rusimp.su>
2017-07-01 17:15:12 +0300

    almost there

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function jsonpQuote(){
     $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=getQuote&?callback=?");
 }
 function getQuote(response){
-    var quoteText = "<i class=\"fa fa-quote-left\" aria-hidden=\"true\"></i>&nbsp;"+response.quoteText+"&nbsp;<i class=\"fa fa-quote-right\" aria-hidden=\"true\"></i>";
+    var quoteText = "<i class=\"fa fa-quote-left\" aria-hidden=\"true\"></i>&nbsp;"+response.quoteText.trim();
     var quoteAuthor = response.quoteAuthor;
     $(".quoteText").html(quoteText);
     $(".quoteAuthor").html(quoteAuthor);
