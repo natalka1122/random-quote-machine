120f2cc92afd27446a1f74792bfcd223c2942f74
unknown <user@kalashnikova.rusimp.su>
2017-07-01 14:35:07 +0300

    adding jsonp source

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 $( document ).ready(function() {
     $("#newQuote").on("click", function() {
-        $.getJSON("https://talaikis.com/api/quotes/random/", function(json) {
+        $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=getQuote&?callback=?");
+    });
+});
+function getQuote(response){
             var html = "Hi json";
             // Only change code below this line.
             
             // Only change code above this line.
             $(".quoteText").html(html);
-        });
-    });
-});
+  console.log (response);
+}
