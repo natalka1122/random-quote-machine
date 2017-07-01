840bb451329ed33ad6980da412ad12271b6c4fc4
unknown <user@kalashnikova.rusimp.su>
2017-07-01 16:29:51 +0300

    fontsize

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 $( document ).ready(function() {
+    var dpt = window.devicePixelRatio;
+    var widthM = window.screen.width;
+    var widthH = window.screen.height;
+    if (widthM*dpt < 976) {
+        document.write('<meta name="viewport" content="width=600, user-scalable=yes">');
+    }
+    
     jsonpQuote();
     $("#newQuote").on("click", jsonpQuote);
 });
