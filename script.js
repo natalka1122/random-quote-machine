d60dc3ad6d495d4529726f94da7a20e9e365dae5
unknown <user@kalashnikova.rusimp.su>
2017-07-01 17:32:53 +0300

    seemes done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {    
+$( document ).ready(function() {
     jsonpQuote();
     $("#newQuote").on("click", jsonpQuote);
 });
@@ -6,8 +6,12 @@ function jsonpQuote(){
     $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=getQuote&?callback=?");
 }
 function getQuote(response){
-    var quoteText = "<i class=\"fa fa-quote-left\" aria-hidden=\"true\"></i>&nbsp;"+response.quoteText.trim();
+    var quoteText = response.quoteText.trim();
     var quoteAuthor = response.quoteAuthor;
-    $(".quoteText").html(quoteText);
+    var twitterLink="https://twitter.com/intent/tweet?button_hashtag=quote&text="
+    $(".quoteText").html("<i class=\"fa fa-quote-left\" aria-hidden=\"true\"></i>&nbsp;"+quoteText);
     $(".quoteAuthor").html(quoteAuthor);
+    $(".quoteTwitter").prop("href",twitterLink+"\""+quoteText+"\" "+quoteAuthor);
 }
+
+//https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=%22They%20call%20me%20Mister%20Tibbs!%22%20In%20the%20Heat%20of%20the%20Night
